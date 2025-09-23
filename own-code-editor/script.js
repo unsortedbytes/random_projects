d867c37ee398d155f988f51b3041fa4c1f5105f2
unsortedkgpian <theadityakumar2810@gmail.com>
2025-09-23 13:45:21 +0530

    fix bugs

--- own-code-editor/script.js
+++ own-code-editor/script.js
@@ -1,2 +1,14 @@
 // console.log("Hello world")
 // alert("Connected to js")
+
+// =========== UTILITIES ======================
+
+const $ = s => document.querySelector(s);
+const $$ = s => Array.from(document.querySelectorAll(s));// Node-list 
+
+const out = $("#output");
+const preview = $("#preview");
+const STORAGE_KEY = "code-unsortedbytes-in";
+
+const escapeHtml = s => 
+    String(s)
